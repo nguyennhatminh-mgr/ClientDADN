3ebcac9fea34b8c3135fa936d1def01af8881071
nhatminh123 <nguyennhatminh12a1@gmail.com>
2020-05-10 23:56:26 +0700

    add components

---
+++ src/components/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function Hello(){
+    return (
+        <View>
+            <Text>Đây chỉ là component để test</Text>
+        </View>
+    );
+}
