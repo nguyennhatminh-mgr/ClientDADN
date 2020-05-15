07e7c379ab487e0cd301426a7bbaf26689b44921
lucle99 <48874410+lucle99@users.noreply.github.com>
2020-05-15 11:30:44 +0700

    Create sample.js

---
+++ src/data/sample.js
@@ -0,0 +1,30 @@
+var roomList =[{
+    roomID: 'H1-101',
+    owner: 'Le Tan Luc',
+    brightness: '100',
+    lightOn: 4,
+    numOfLight: 5
+    },
+    {
+      roomID: 'H1-102',
+      owner: 'Nguyen Nhat Minh',
+      brightness: '80',
+      lightOn: 4,
+      numOfLight: 5
+    },
+    {
+      roomID: 'H1-103',
+      owner: 'Le Cong Linh',
+      brightness: '70',
+      lightOn: 4,
+      numOfLight: 5
+    },
+    {
+      roomID: 'H1-104',
+      owner: 'Chu Van Hoai Lam',
+      brightness: '110',
+      lightOn: 4,
+      numOfLight: 5
+    }
+  ];
+  export default roomList;
