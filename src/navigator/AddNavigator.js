911649441ffb3ec3c3cd86a666f09cb2191bbd31
Lam <lamkhmt07@gmail.com>
2020-05-15 17:31:41 +0700

    addSensor screen

--- src/navigator/AddNavigator.js
+++ src/navigator/AddNavigator.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Menu from '../screens/addObject';
+import AddSensor from '../screens/addSensor';
+
 
 const Stack = createStackNavigator();
 
 export default function AddNavigator(){
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Add"
+            <Stack.Screen name="AddMenu"
             component={Menu}
+            options={{headerShown:false}}/>
+        
+            <Stack.Screen name="AddSensor"
+            component={AddSensor}
             options={{headerShown:false}}/>
         </Stack.Navigator>
     );
