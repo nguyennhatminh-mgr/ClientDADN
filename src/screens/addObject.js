f24dd6878fd08431544566a7056e84237d4cd67d
glxdsunname <lamkhmt07@gmail.com>
2020-05-15 14:47:49 +0700

    modify add screen

--- src/screens/addObject.js
+++ src/screens/addObject.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {View, Text ,TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text ,TouchableOpacity, StyleSheet, ImageBackground,Dimensions} from 'react-native';
+const { width, height } = Dimensions.get('window');
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import lightSensor from '../assets/images/lightSensor.png';
+import light from '../assets/images/light.png';
+import room from '../assets/images/room.png';
+import building from '../assets/images/building.png';
 
-const Items = ({icon, type, onClick}) => {
+const Items = ({icon, type, onClick, imageSrc}) => {
     return (
-        <TouchableOpacity onPress={onClick}>     
-            <View style={Styles.itemContainer}>
-                <View style={Styles.iconBorder}>
-                    <FontAwesome name={icon} size={20}/>
-                </View>
-                <Text style={Styles.Text}>them {type}</Text>
-                <FontAwesome name="angle-right" size={26}/>
-            </View>
+        <TouchableOpacity onPress={onClick} style={Styles.Items}>
+            
+                <ImageBackground source={imageSrc} style={Styles.image}> 
+                    <FontAwesome name={icon} size={30}/>
+                    <Text style={Styles.Text}>Add {type}</Text>
+                    <FontAwesome name="angle-right" size={30}/>
+                </ImageBackground>     
+              
         </TouchableOpacity>
     );
 }
@@ -19,10 +24,17 @@ const Items = ({icon, type, onClick}) => {
 export default function AddObjectScreen({navigation}){
     return (
         <View style={Styles.listContainer}>
-            <Items icon="rss-square" type="cam bien" onClick={()=>navigation.navigate('AddSensor')}/>
-            <Items icon="lightbulb-o" type="den"/>
-            <Items icon="building" type="phong"/>
-            <Items icon="building" type="toa nha"/>
+
+            <View style={Styles.header}>
+                <FontAwesome name="plus-circle" size={height/4} style={Styles.header_icon}/>
+            </View>
+
+            <View style={Styles.listItem}>
+                <Items icon="rss-square" type="Sensor" onClick={()=>navigation.navigate('AddSensor')} imageSrc={lightSensor}/>
+                <Items icon="lightbulb-o" type="Light" imageSrc={light}/>
+                <Items icon="building" type="Room" imageSrc={room}/>
+                <Items icon="building" type="Building" imageSrc={building}/>
+            </View> 
         </View>       
     );
 }
@@ -35,33 +47,39 @@ const Styles = StyleSheet.create({
         flex:1,
         justifyContent:"center"
     },
-    itemContainer:{
-        backgroundColor:"#FFF",
-        borderRadius:25,
-        height:40,
-        display:"flex",
+    header: {
+        flex: 3,
+        backgroundColor: "#cceabb",
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
         flexDirection:"row",
+        backgroundColor:"#f7f7f7",
         alignItems:"center",
         justifyContent:"center",
-        shadowColor:"black",
-        // ShadowRoot:"",
-        marginBottom:30,
-        marginHorizontal:"30%",
-        width:"50%"
+        shadowColor:'#000',
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        shadowOffset: {width:2, height:2},
+        elevation: 6,
+        height:"100%",
+        width:"100%"
+      },
+    header_icon: {
+        // position: "absolute",
+        // bottom: 20
     },
+    listItem:{
+        flex:6,
+    },
+    Items:{height:"25%",borderRadius:20, width:"100%"},
     Text:{
         paddingLeft:5,
-        fontSize:20,
-        width:"70%"
+        fontSize: height/30,
+        width:"80%"
     },
-    iconBorder:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        // backgroundColor:"#000",
-        height: 10,
-        width: 10,
-        borderRadius: 10,  
-    }
 
 })
