9bc93adb2173fcc13188ac782cc5ad010ae7cd78
long-lehoang <long.bk.khmt@gmail.com>
2020-05-16 12:39:32 +0700

    nothing

--- src/screens/HistoryOp.js
+++ src/screens/HistoryOp.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text ,TouchableOpacity, StyleSheet} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Items = ({type,onClick}) => {
     return (
