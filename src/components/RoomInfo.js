84822867d3ab4e82c468f920040b574abc074d17
lucle99 <48874410+lucle99@users.noreply.github.com>
2020-06-18 09:48:52 +0700

    Update RoomInfo.js

--- src/components/RoomInfo.js
+++ src/components/RoomInfo.js
@@ -1,97 +1,61 @@
 import React from 'react';
-import {TouchableOpacity ,StyleSheet, Text, View, Image, ImageBackground, Dimensions, RefreshControl } from 'react-native';
-
+import { TouchableOpacity ,StyleSheet, Text, View,ScrollView, Image, ImageBackground, Dimensions, RefreshControl } from 'react-native';
+import ItemInRoomInfo from './ItemInRoomInfo';
 export default class RoomInfo extends React.Component{
+  _isMounted= false;
   constructor(props){
     super(props);
     this.state ={
       data :[],
-      id : this.props.userID
+      userID : this.props.userID, 
     }
   }
-  // convertDate =(dateTime)=>
-  //    {
-  //       let time = new Date(dateTime);
-  //       let timeString = time.getDate() + "/"
-  //        + (time.getMonth()+1)  + "/" 
-  //        + time.getFullYear() + " , "  
-  //        + time.getHours() + ":"  
-  //        + time.getMinutes() + ":" 
-  //        + time.getSeconds();
-  //       return timeString;
-  //    }   
-  // getData = async (userID)=>{
-  //   fetch(`http://192.168.1.102:3000/viewroom/${this.state.id}`)
-  //   .then((response)=> response.json())
-  //   .then((responseJson)=>{
-  //       if(this.isFreshing){
-  //         this.setState({data: responseJson});
-  //       }
-  //   })
-  //   .catch(err=> console.log(err));
-  // }
-  //  componentWillMount(){
-  //    this.isFreshing = false;
-  //  }
-  // handleRefresh = () =>{
-  //    this.isFreshing = true;
-  //    this.getData();
-  // }
-  // componentDidMount(){
-  //   this.isFreshing = true;
-  //   if(this.isFreshing){
-  //     this.getData(this.props.userID);
-  //  }
-  // }
-    handleViewDetail = (navigation, userID)=>
-    {
-      navigation.navigate('ControlNavigator');
-    }
+  convertDate =(dateTime)=>
+     {
+        let time = new Date(dateTime);
+        let timeString = time.getDate() + "/"
+         + (time.getMonth()+1)  + "/" 
+         + time.getFullYear() + " , "  
+         + time.getHours() + ":"  
+         + time.getMinutes() + ":" 
+         + time.getSeconds();
+        return timeString;
+     }
+  handleViewDetail = (navigation, userID)=>
+  {
+    navigation.navigate('Control', {screen: 'ListDevice', params: {userID: userID}});
+  }
+    
     render(){
       let time = new Date().getHours();
+      console.log(this.props.data);
       const {navigation, handleVD} = this.props;
       let urlBackground  = (time>= 18)? require("../assets/images/nightbackground.jpg") : require("../assets/images/daybackground.jpg");
         return(
           <View style = {styles.container}>
               <ImageBackground source = {urlBackground}
               style = {styles.background}>
-              <View style={{
-                          flex: 1,
+              <View style={{flex: 1,
                           justifyContent: 'center',
                           alignItems: 'center'}}>
-              <Text style={styles.header}>Last refreshed : {this.props.time}</Text>
+              <Text style={styles.header}>Last refreshed : {this.convertDate(new Date().getTime())}</Text>
             </View>
             <View style ={styles.user}>
-              <View style={{flex:2,}}>
-                <Image style= {styles.big_icon} source = {require('../assets/images/icon_house.png')}/>
-              </View>
-              <View style ={{flex: 5}}>
-                <Text style ={styles.text}>Room ID: {this.props.roomID}</Text>
-                <Text style ={styles.decription}>Owner: {this.props.owner}</Text>
-              </View>
+
+              <Text style ={{fontSize: 30*standarWidth/screenWidth,
+                      color: '#1aaa1a',
+                      fontWeight:'bold'}}>List Devices</Text>
             </View>
-            {/* <RefreshControl freshing = {this.isFreshing} onRefresh = {this.handleRefresh()}/> */}
-            <View style ={{flex:5, alignItems: 'center', justifyContent:'center'}}>
-              <View style={styles.specification}>
-                  <View style={{flex: 2}}>
-                    <Image style={styles.image}
-                    source ={require('../assets/images/icon_light.png')} ></Image>
-                  </View>
-                  <View style ={{flex: 5}}>
-              <Text style ={styles.decription}>Device ID: {this.props.deviceID}</Text>
-                  </View>
-                </View>
-              <View style={styles.specification}>
-                <View style={{flex: 2}}>
-                  <Image style={styles.image} source ={require('../assets/images/icon_brightness.png')} />
-                </View>
-                <View style ={{flex: 5}}>
-                    <Text style ={styles.decription}>Brightness : {this.props.brightness}</Text>
-                </View>
-              </View>
+            <View style={{alignItems:'center', flex: 5, padding: 10}}>
+            <ScrollView>
+              {
+                this.props.data.map((item, index)=> <ItemInRoomInfo key = {index} id= {item.deviceID} type ={item.type} value= {item.value} roomID ={item.roomName} time ={this.convertDate(item.time)}/>)
+              }
+            </ScrollView>
             </View>
+            <View style={{flex: 2}}>
             <View style={{flex: 1, justifyContent: 'center', alignItems:'center'}}>
-              <TouchableOpacity style = {styles.button} onPress ={()=>handleVD(navigation, this.state.id)}>
+              <TouchableOpacity style = {styles.button} onPress ={()=>this.handleViewDetail(navigation, this.props.userID)}>
                     <Text style ={styles.title_button}>View detail</Text>
               </TouchableOpacity>
             </View>
@@ -100,6 +64,7 @@ export default class RoomInfo extends React.Component{
              >
                     <Text style ={styles.title_button}>Back</Text>
               </TouchableOpacity>
+            </View>
             </View>
             <Text style={styles.footer}>Phần mềm quản lý hệ thống chiếu sáng</Text>
             </ImageBackground>
@@ -178,7 +143,7 @@ export default class RoomInfo extends React.Component{
     footer:{
       color: 'gray', 
       textAlign:'center', 
-      margin:10*standarWidth/screenWidth
+      marginBottom:10*standarWidth/screenWidth
     },
     title_button:{
       color: 'white', 
@@ -190,3 +155,4 @@ export default class RoomInfo extends React.Component{
       color: 'white'
     }
   });
+
