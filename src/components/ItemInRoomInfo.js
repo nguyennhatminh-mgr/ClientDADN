347ae49832356602e9ba0edb6816b7d61e0a9039
lucle99 <48874410+lucle99@users.noreply.github.com>
2020-06-29 00:19:25 +0700

    Update ItemInRoomInfo.js

--- src/components/ItemInRoomInfo.js
+++ src/components/ItemInRoomInfo.js
@@ -4,7 +4,7 @@ import {Text, View, StyleSheet, Image, Dimensions} from 'react-native';
 
 export default class ItemInRoomInfo extends Component{  
     render(){
-        let urlIcon = (this.props.type == "sensor")? require('../assets/images/iconSensor.png') : require('../assets/images/icon_light.png');
+        let urlIcon = (this.props.type == "Sensor")? require('../assets/images/iconSensor.png') : require('../assets/images/icon_light.png');
         return(
             <View style ={styles.container}>
                 <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
@@ -14,8 +14,8 @@ export default class ItemInRoomInfo extends Component{
                             source ={urlIcon} ></Image>
                 </View>
                 <View style ={{flex: 8, alignItems: 'flex-start', justifyContent: 'center'}}>
-                    <Text style ={styles.decription}>Room ID: {this.props.roomID}</Text>
-                    <Text style ={styles.decription}>Device: {this.props.type} {this.props.id}</Text>
+                    <Text style ={styles.decription}>Device ID: {this.props.id}</Text>
+                    <Text style ={styles.decription}>Type: {this.props.type}</Text>
                     <Text style ={styles.decription}>Value: {this.props.value}</Text>
                     <Text style ={styles.decription}>Received time: {this.props.time}</Text>
                 </View>
@@ -31,5 +31,5 @@ const styles = StyleSheet.create({
         fontSize: 15,
         color: '#1aaa1a',
     },
-    container:{width: screenWidth, height: 100, flexDirection: "row", backgroundColor: 'white'}
+    container:{width: screenWidth, height: 120, flexDirection: "row", backgroundColor: 'white'}
 });
