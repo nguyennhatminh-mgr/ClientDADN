a622a7e9f3e039a682ad9d54a15b72b60e5b330c
lucle99 <48874410+lucle99@users.noreply.github.com>
2020-06-29 22:31:47 +0700

    Update ItemInRoomInfo.js

--- src/components/ItemInRoomInfo.js
+++ src/components/ItemInRoomInfo.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import * as Constant from '../constant/Constant';
 import {Text, View, StyleSheet, Image, Dimensions} from 'react-native';
 
 
 export default class ItemInRoomInfo extends Component{  
     render(){
-        let urlIcon = (this.props.type == "Sensor")? require('../assets/images/iconSensor.png') : require('../assets/images/icon_light.png');
+        let urlIcon = (this.props.type == "Sensor" ||this.props.type == "sensor"||this.props.type == "SENSOR")? require('../assets/images/iconSensor.png') : require('../assets/images/icon_light.png');
         return(
             <View style ={styles.container}>
-                <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
-                    <Image style={{height : 40, 
-                            width:40, 
-                            backgroundColor:'transparent'}}
+                <View style={styles.container1}>
+                    <Image style={styles.image}
                             source ={urlIcon} ></Image>
                 </View>
-                <View style ={{flex: 8, alignItems: 'flex-start', justifyContent: 'center'}}>
+                <View style ={styles.container3}>
                     <Text style ={styles.decription}>Device ID: {this.props.id}</Text>
                     <Text style ={styles.decription}>Type: {this.props.type}</Text>
                     <Text style ={styles.decription}>Value: {this.props.value}</Text>
@@ -24,12 +23,29 @@ export default class ItemInRoomInfo extends Component{
     }
     
 }
-const screenWidth =  Dimensions.get("window").width;
-const screenHeight =  Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
     decription:{
-        fontSize: 15,
+        fontSize: 15*Constant.screenHeight/748,
         color: '#1aaa1a',
     },
-    container:{width: screenWidth, height: 120, flexDirection: "row", backgroundColor: 'white'}
+    container:{
+        width: Constant.screenWidth, 
+        height: 120*Constant.screenHeight/748, 
+        flexDirection: "row", 
+        backgroundColor: 'white', 
+        padding: 10*Constant.screenHeight/748
+    },
+    container1:{
+        flex: 2, 
+        alignItems: 'center', 
+        justifyContent: 'center'},
+    container3:{
+        flex: 8, 
+        alignItems: 'flex-start', 
+        justifyContent: 'center'},
+    image:{
+        height : 45*Constant.screenHeight/748, 
+        width:45*Constant.screenHeight/748, 
+        backgroundColor:'transparent'}
 });
